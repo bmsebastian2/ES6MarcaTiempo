0bb18470f7238fcaad3245b07617db0955271b0f
Sebastian <55502717+bmsebastian2@users.noreply.github.com>
2023-08-22 15:14:12 +0000

    SE anexo style, y se send al get index.html

--- server.mjs
+++ server.mjs
@@ -1,1 +1,20 @@
+import * as url from 'url';
+import express from 'express';
+import path from 'path';
+//-------------------------------------------------------------
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+//-------------------------------------------------------------
 console.log('ES6MarcaTiempo')
+
+const app = express()
+app.use(express.static(path.join( __dirname + '/style')))
+const PORT  = process.env.PORT ?? 8080
+
+app.get('/',(req,res)=>{
+    res.sendFile( path.join(__dirname+'views/index.html') )
+})
+
+app.listen(PORT,()=>console.log(`Puerto Activo: ${PORT}`))
+
+
